648210b850a9e8c06d4c2c8a32c61eb204d5d657
Max Karpushonok <max.karpushonok@gmail.com>
2020-01-30 13:01:47 +0300

    Changed image_url xpath, added .text

--- Test.rb
+++ Test.rb
@@ -32,14 +32,14 @@ products_url.each do |url|
   html = Curl.get(url)
   doc = Nokogiri::HTML(html.body)
   name = doc.xpath('//h1').text
-  image_url = doc.xpath("//img[@id='bigpic']/@src")
-  prices = doc.xpath("//label[contains(@class, 'label_comb_price')]").each do |l|
+  image_url = doc.xpath("//img[@id='bigpic']/@src").text
+  doc.xpath("//label[contains(@class, 'label_comb_price')]").each do |l|
     type = l.css('.radio_label').text
     price = l.css('.price_comb').text.split('/').first
     products.push('name'=> name + ' ' + type, 'price' => price, 'image_url' => image_url)
   end
 end
 
-
+puts products
 
 puts 'End...'
