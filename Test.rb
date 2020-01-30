d4c1903cdbd3571c0fbf017ebbbb8074bbf01f15
Max Karpushonok <max.karpushonok@gmail.com>
2020-01-30 16:11:27 +0300

    Changing parsing pages for a single-price page

--- Test.rb
+++ Test.rb
@@ -8,7 +8,7 @@ require 'csv'
 # URL = gets.chomp
 # puts 'Enter csv file name:'
 # file = gets.chomp
-URL = 'https://www.petsonic.com/snacks-higiene-dental-para-perros/'
+URL = 'https://www.petsonic.com/semihumedos-para-perros/'
 FILE = 'file'
 
 #Parsing category page
@@ -25,7 +25,6 @@ end
 #TODO debug
 # puts products_url
 
-#TODO pages with one price
 #Parsing pages
 puts 'Start parsing pages...'
 products = []
@@ -34,11 +33,18 @@ products_url.each do |url|
   doc = Nokogiri::HTML(html.body)
   name = doc.xpath('//h1').text
   image_url = doc.xpath("//img[@id='bigpic']/@src").text
-  doc.xpath("//label[contains(@class, 'label_comb_price')]").each do |l|
-    type = l.css('.radio_label').text
-    price = l.css('.price_comb').text.split('/').first
-    products.push('name'=> name + ' - ' + type, 'price' => price, 'image_url' => image_url)
+  prices = doc.xpath("//label[contains(@class, 'label_comb_price')]")
+  if prices.size > 0 then
+    prices.each do |l|
+      type = l.css('.radio_label').text
+      price = l.css('.price_comb').text.split('/').first
+      products.push('name' => name + ' - ' + type, 'price' => price, 'image_url' => image_url)
+    end
+  else
+    price = doc.xpath("//span[@id = 'our_price_display']").text
+    products.push('name' => name, 'price' => price, 'image_url' => image_url)
   end
+
 end
 
 #TODO debug
