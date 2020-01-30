84fdc62af431d0715eeede2a7d2788e01fc54b7f
Max Karpushonok <max.karpushonok@gmail.com>
2020-01-30 10:10:14 +0300

    Parsing category page

--- Test.rb
+++ Test.rb
@@ -11,4 +11,18 @@ require 'csv'
 url = 'https://www.petsonic.com/snacks-higiene-dental-para-perros/'
 file = 'file'
 
+#Parsing category page
+puts 'Start open category page...'
+html = Curl.get(url)
+
+#TODO recursion def
+puts 'Start parsing category page...'
+products_url = []
+doc = Nokogiri::HTML(html.body)
+doc.css('a.product_img_link').each do |a|
+  products_url.push(a['href'])
+end
+#TODO debug
+# puts products_url
+
 
