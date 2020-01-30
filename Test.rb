fc861f8bc5fdb1c79a453b735f6de69d9acfdd34
Max Karpushonok <max.karpushonok@gmail.com>
2020-01-30 10:03:55 +0300

    Entry data

--- Test.rb
+++ Test.rb
@@ -1,2 +1,14 @@
 # Test ruby
+require 'curb'
+require 'nokogiri'
+require 'csv'
+
+#Entry data
+# puts 'Enter category URL:'
+# url = gets.chomp
+# puts 'Enter csv file name:'
+# file = gets.chomp
+url = 'https://www.petsonic.com/snacks-higiene-dental-para-perros/'
+file = 'file'
+
 
